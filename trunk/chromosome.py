11a78486b74dfd01b78e34758b619d2f5a4289c1
Akdom2001 <Akdom2001@8ba46332-c897-11dd-8772-d1b041bf0e37>
2008-12-12 22:00:40 +0000

    Initial Commit

---
+++ trunk/chromosome.py
@@ -0,0 +1,62 @@
+import random
+class Chromosome():
+	def __init__(self, XMAX, YMAX):
+		#Point max & min
+		self.PMAX = 10
+		self.PMIN = 3
+		
+		border = 50
+		self.XMAX = XMAX + border
+		self.YMAX = YMAX + border
+		self.XMIN = -border
+		self.YMIN = -border
+		
+		self.outline = []
+		self.fill_rgb = (random.randint(0,255), random.randint(0,255), 
+			random.randint(0,255))
+		#Opacity can evolve, but start is fixed
+		self.fill_a = 10 #random.randint(0, 255)
+		
+		for i in xrange(random.randint(self.PMIN,self.PMAX)):
+			self.outline.append((random.randint(self.XMIN, self.XMAX), 
+				random.randint(self.YMIN, self.YMAX)))
+	
+	def insert(self):
+		if len(self.outline) < self.PMAX:
+			self.outline.insert(random.randint(0, len(self.outline)), (random.randint(self.XMIN, self.XMAX), random.randint(self.YMIN, self.YMAX)))
+	
+	def delete(self):
+		if self.PMIN < len(self.outline):
+			self.outline.remove(random.choice(self.outline))
+	
+	def adjust_color(self):
+		color_adjust = 20
+		
+		red_adjust, green_adjust, blue_adjust, alpha_adjust = 0,0,0,0
+		
+		decide = random.random()
+		if decide < .3:
+			red_adjust = random.randint(-color_adjust,color_adjust)
+		elif decide < .6:
+			green_adjust = random.randint(-color_adjust,color_adjust)
+		elif decide < .9:
+			blue_adjust = random.randint(-color_adjust,color_adjust)
+		
+		if decide <= .5:
+			alpha_adjust = random.randint(-10, 10)
+		
+		self.fill_rgb = (max(0, min(255, self.fill_rgb[0] + red_adjust)),max(0, min(255, self.fill_rgb[1] + green_adjust)),max(0, min(255, self.fill_rgb[2] + blue_adjust)))
+		
+		self.fill_a = max(0, min(255, self.fill_a + alpha_adjust))
+	
+	def adjust_point(self):
+		distance_adjust = 20
+		x_adjust = random.randint(-distance_adjust,distance_adjust)
+		y_adjust = random.randint(-distance_adjust,distance_adjust)
+		
+		point_index = random.randint(0, len(self.outline)-1)
+		
+		self.outline[point_index] = (max(self.XMIN, min(self.XMAX, self.outline[point_index][0] + x_adjust)), max(self.YMIN, min(self.YMAX, self.outline[point_index][1] + y_adjust)))
+	
+	def __str__(self):
+		return "("+ str(self.fill_rgb) +","+ str(self.fill_a) +")<"+ str(self.outline) +">"
---
+++ trunk/genome.py
@@ -0,0 +1,86 @@
+import Image, ImageDraw, ImageChops, ImageStat
+import random, copy
+from chromosome import Chromosome
+class Genome():
+	modifiers = ("insert", "delete", "adjust_color", "adjust_point")
+	XMAX = 0
+	YMAX = 0
+	
+	def __init__(self, XMAX, YMAX):
+		self.XMAX = XMAX
+		self.YMAX = YMAX
+		
+		self.last_draw = None
+		
+		self.chromosomes = []
+		for i in xrange(50):
+			self.chromosomes.append(Chromosome(self.XMAX, self.YMAX))
+	
+	def gene_transfer(self, second):
+		second.last_draw = None
+		second.chromosomes = []
+		for i in self.chromosomes:
+			second.chromosomes.append(copy.deepcopy(i))
+	
+	def mutate(self, rate):
+		#Adjust multiple chromosomes per iteration
+		#rate is the maximum percentage of chromosomes to adjust
+		for i in random.sample(self.chromosomes, random.randint(0, int((len(self.chromosomes)-1)*(rate/100))+1)):
+			eval("i."+ random.choice(self.modifiers) +"()")
+		
+		#Adjust multiple chromosomes per iteration
+#		eval("self.chromosomes["+ str(random.randint(0,len(self.chromosomes)-1)) +"]."+ random.choice(self.modifiers) +"()")
+		
+		#Polygon position and existence alteration
+		decide = random.random()
+		if decide < .2:
+			move_index = random.randint(0,len(self.chromosomes)-1)
+			to_be_moved = self.chromosomes[move_index]
+			self.chromosomes.remove(to_be_moved)
+			self.chromosomes.append(to_be_moved)
+#		elif decide < .22:
+#			self.delete_chromosome()
+#		elif decide < .24:
+#			self.add_chromosome()
+   
+	def delete_chromosome(self):
+		del self.chromosomes[random.randint(0,len(self.chromosomes)-1)]
+   	
+	def add_chromosome(self):
+		self.chromosomes.append(Chromosome(self.XMAX, self.YMAX))
+	
+	def draw(self): 
+		base_layer = Image.new('RGBA',(self.XMAX,self.YMAX))
+		color_layer = Image.new('RGBA',(self.XMAX, self.YMAX))
+		color_layer_draw = ImageDraw.Draw(color_layer)
+		for chromosome in self.chromosomes:
+			color, opacity, points = chromosome.fill_rgb, chromosome.fill_a, chromosome.outline
+			color_layer_draw.rectangle((0,0, self.XMAX,self.YMAX), color)
+			alpha_mask = Image.new('L',(self.XMAX,self.YMAX), 0)
+			alpha_mask_draw = ImageDraw.Draw(alpha_mask)
+			alpha_mask_draw.polygon(points,fill=opacity)
+			base_layer = Image.composite(color_layer,base_layer,alpha_mask)
+		self.last_draw = base_layer
+		
+		return base_layer
+	
+	def diff(self, base):
+		diffIm = ImageChops.difference(self.draw(), base)
+		diffStat = ImageStat.Stat(diffIm)
+		difference = sum(diffStat.sum) #stat.sum returns a list of each color sum
+		return difference
+#		my_pixels = self.draw().load()
+#		
+#		fitness = 0
+#		for y in xrange(self.YMAX):
+#			for x in xrange(self.XMAX):
+#				c1 = base[x, y]
+#				c2 = my_pixels[x, y]
+#				
+#				del_red = c1[0] - c2[0]
+#				del_green = c1[1] - c2[1]
+#				del_blue = c1[2] - c2[2]
+#				
+#				pixel_fitness = del_red*del_red + del_green*del_green + del_blue*del_blue
+#				fitness += pixel_fitness
+#		return fitness
