221ea3473c50f4210d5ab5e25fe01cf056989e90
alexjray.ncsu <alexjray.ncsu@8ba46332-c897-11dd-8772-d1b041bf0e37>
2008-12-14 05:25:12 +0000

    new library setup

--- trunk/genimagef/fitness.py
+++ trunk/genimagef/fitness.py
@@ -2,22 +2,27 @@
 #  Fitness Function
 #  This is how the program decides if a genome is fit
 import Image, ImageDraw, ImageChops, ImageStat
-from __main__ import ImageWidth, ImageHeight, basecolor
+from genetic_imager import ImageWidth, ImageHeight, background_color, color_mode
 
-def draw(DNA):
-  im = Image.new('L',(ImageWidth,ImageHeight),basecolor)
-  for polygon in DNA:
+def draw_genome(genome):
+  genome_image = Image.new(color_mode,(ImageWidth,ImageHeight),background_color)
+  for polygon in genome:
     color, opacity, points = polygon
     if (len(points) >= 3):
-      im2 = Image.new('L',(ImageWidth,ImageHeight), color)
-      mask = Image.new('L',(ImageWidth,ImageHeight), 0)
-      maskDraw = ImageDraw.Draw(mask)
-      maskDraw.polygon(points, fill=opacity) 
-      im = Image.composite(im2,im,mask)
-  return im
+      color_mask = Image.new(color_mode,(ImageWidth,ImageHeight), color)
+      polygon_mask = Image.new(color_mode,(ImageWidth,ImageHeight), 0)
+      ImageDraw.Draw(polygon_mask).polygon(points, fill=opacity) 
+      genome_image = Image.composite(color_mask,genome_image,polygon_mask)
+  return genome_image
 
-def diff(newImg, baseImg):
-  diffIm = ImageChops.difference(newImg, baseImg)
-  diffStat = ImageStat.Stat(diffIm)
-  diffnum = sum(diffStat.sum2) #stat.sum returns a list of each color sum**2
-  return diffnum
+def image_difference(genome_image, base_image):
+   """
+   Image Difference
+   takes two images and returns the differece
+   calculated per pixel by taking the sum of the sqaured difference of each color
+   and summing all of these over the whole image
+   """
+   difference_image = ImageChops.difference(genome_image, base_image) 
+   difference_statistics = ImageStat.Stat(difference_image) # statistics for difference_image
+   difference_value = sum(difference_statistics.sum2) #stat.sum returns a list of each color sum**2
+   return difference_value
