445f0ed80d3f0535d0d819afc27f0f3b38d6eed9
Akdom2001 <Akdom2001@8ba46332-c897-11dd-8772-d1b041bf0e37>
2008-12-14 20:45:52 +0000

    Re-did some layout, mucked with mutation rates

--- trunk/genimagec/chromosome.py
+++ trunk/genimagec/chromosome.py
@@ -1,16 +1,19 @@
 import random
 class Chromosome():
-	RANGE = 0
 	XMAX = 0
 	YMAX = 0
+	RANGE = 1
 	EDGE = 50
 	PMAX = 10
 	PMIN = 3
-	MUTATORS = ("add_point", \
-				"insert_point", \
+	MUTATORS = ("insert_point", \
 				"delete_point", \
 				"adjust_point", \
-				"adjust_color")
+				"adjust_color", \
+				"adjust_opacity")
+	#Adjustment settings
+	color_adjust = 20
+	alpha_adjust = 20
 	
 	def __init__(self, blank=False):
 		self.outline = []
@@ -23,10 +26,12 @@ class Chromosome():
 			self.fill_rgb = (random.randint(0,255), random.randint(0,255), 
 				random.randint(0,255))
 			#Opacity can evolve, but start is fixed
-			self.fill_a = 10 #random.randint(0, 255)
+			self.fill_a = random.randint(10, 20)
 			
+			center = (random.randint(self.XMIN, self.XMAX), 
+						random.randint(self.YMIN, self.YMAX))
 			for i in xrange(random.randint(self.PMIN,self.PMAX)):
-				self.add_point()
+				self.add_point(center)
 	
 	def __str__(self):
 		return "("+ str(self.fill_rgb[0]) + \
@@ -45,7 +50,8 @@ class Chromosome():
 	
 	@classmethod
 	def set_range(self, range):
-		self.RANGE = 100./range
+		self.RANGE = range/100.
+		print self.RANGE
 	
 	def from_string(self, serial):
 		self.outline = []
@@ -56,32 +62,56 @@ class Chromosome():
 		temp = temp.replace(" ", "")
 		intermediate = temp.split(",")
 		
-		self.fill_rgb = tuple(intermediate[0:3])
-		self.fill_a = intermediate[3]
+		self.fill_rgb = tuple((int(i) for i in intermediate[0:3]))
+		self.fill_a = int(intermediate[3])
 		self.outline = zip((int(i) for i in intermediate[4::2]), 
 							(int(i) for i in intermediate[5::2]))
 	
 	def mutate(self):
 		eval("self."+ random.choice(self.MUTATORS) +"()")
 	
-#Mutations
-	def insert_point(self):
-		XMAX = int(self.RANGE * self.XMAX)
-		XMIN = int(self.RANGE * self.XMIN)
-		YMAX = int(self.RANGE * self.YMAX)
-		YMIN = int(self.RANGE * self.YMIN)
+####Mutations####
+
+##Shape alterations##
+	def insert_point(self, center=False):
+#		if center:
+#			assert type(center) == tuple and len(center) == 2
+#			XMAX = center[0] + abs(int(self.RANGE * (self.XMAX - center[0])))
+#			XMIN = center[0] - abs(int(self.RANGE * (self.XMIN + center[0])))
+#			YMAX = center[1] + abs(int(self.RANGE * (self.YMAX - center[1])))
+#			YMIN = center[1] - abs(int(self.RANGE * (self.YMIN + center[1])))
+#		else:
+#			XMAX = self.XMAX
+#			XMIN = self.XMIN	
+#			YMAX = self.YMAX
+#			YMIN = self.YMIN	
 		
+		XMAX = self.XMAX
+		XMIN = self.XMIN	
+		YMAX = self.YMAX
+		YMIN = self.YMIN	
 		if len(self.outline) < self.PMAX:
 			self.outline.insert(random.randint(0, len(self.outline)), 
 				(random.randint(XMIN, XMAX),
 					 random.randint(YMIN, YMAX)))
 	
-	def add_point(self):
-		XMAX = int(self.RANGE * self.XMAX)
-		XMIN = int(self.RANGE * self.XMIN)
-		YMAX = int(self.RANGE * self.YMAX)
-		YMIN = int(self.RANGE * self.YMIN)
+	def add_point(self, center=False):
+#		if center:
+#			assert type(center) == tuple and len(center) == 2
+#			XMAX = center[0] + abs(int(self.RANGE * (self.XMAX - center[0])))
+#			XMIN = center[0] - abs(int(self.RANGE * (self.XMIN + center[0])))
+#			YMAX = center[1] + abs(int(self.RANGE * (self.YMAX - center[1])))
+#			YMIN = center[1] - abs(int(self.RANGE * (self.YMIN + center[1])))
+#		else:
+#			XMAX = self.XMAX
+#			XMIN = self.XMIN	
+#			YMAX = self.YMAX
+#			YMIN = self.YMIN	
 		
+		XMAX = self.XMAX
+		XMIN = self.XMIN	
+		YMAX = self.YMAX
+		YMIN = self.YMIN	
 		if len(self.outline) < self.PMAX:
 			self.outline.append((random.randint(XMIN, XMAX),
 				random.randint(YMIN, YMAX)))
@@ -89,28 +119,6 @@ class Chromosome():
 	def delete_point(self):
 		if self.PMIN < len(self.outline):
 			self.outline.remove(random.choice(self.outline))
-	
-	def adjust_color(self):
-		color_adjust = 20
-		
-		red_adjust, green_adjust, blue_adjust, alpha_adjust = 0,0,0,0
-		
-		decide = random.random()
-		if decide < .3:
-			red_adjust = random.randint(-color_adjust,color_adjust)
-		elif decide < .6:
-			green_adjust = random.randint(-color_adjust,color_adjust)
-		elif decide < .9:
-			blue_adjust = random.randint(-color_adjust,color_adjust)
-		
-		if decide <= .5:
-			alpha_adjust = random.randint(-10, 10)
-		
-		self.fill_rgb = (max(0, min(255, self.fill_rgb[0] + red_adjust)),
-						max(0, min(255, self.fill_rgb[1] + green_adjust)),
-						max(0, min(255, self.fill_rgb[2] + blue_adjust)))
-		
-		self.fill_a = max(0, min(255, self.fill_a + alpha_adjust))
 	
 	def adjust_point(self):
 		distance_adjust = 20
@@ -124,4 +132,20 @@ class Chromosome():
 					self.outline[point_index][0] + x_adjust)), 
 			max(self.YMIN, min(self.YMAX, 
 					self.outline[point_index][1] + y_adjust)))
+	
+##Color Alterations##
+	def adjust_color(self):
+		red_adjust = random.randint(-self.color_adjust,self.color_adjust)
+		green_adjust = random.randint(-self.color_adjust,self.color_adjust)
+		blue_adjust = random.randint(-self.color_adjust,self.color_adjust)
+		
+		self.fill_rgb = (max(0, min(255, self.fill_rgb[0] + red_adjust)),
+						max(0, min(255, self.fill_rgb[1] + green_adjust)),
+						max(0, min(255, self.fill_rgb[2] + blue_adjust)))
+		
+	
+	def adjust_opacity(self):
+		alpha_adjust = random.randint(-self.alpha_adjust,self.alpha_adjust)
+		self.fill_a = max(0, min(255, self.fill_a + alpha_adjust))
+	
 	
