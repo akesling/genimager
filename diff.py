f0d839668a95719bb6eaa0d02b41870ef455ab74
Akdom2001 <Akdom2001@8ba46332-c897-11dd-8772-d1b041bf0e37>
2008-12-15 03:20:58 +0000

    Added diff.py script (shows the difference between two images)

--- diff.py
+++ diff.py
@@ -1,6 +1,3 @@
 import Image, ImageChops
 import sys
-original = Image.open(sys.argv[1])
-final = Image.open(sys.argv[2])
-difference = ImageChops.difference(original, final)
-difference.show()
+ImageChops.difference(Image.open(sys.argv[1]), Image.open(sys.argv[2])).show()
